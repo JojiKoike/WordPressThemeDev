f08179ce0707359d7de30b60a9ad7797308da196
Joji Koike <joji.koike@gmail.com>
2017-07-02 23:11:21 +0900

    記事詳細表示基本機能実装完了

--- application/wordpress/wp-content/themes/letsautomatic/single.php
+++ application/wordpress/wp-content/themes/letsautomatic/single.php
@@ -1,7 +1,15 @@
 <?php get_header(); ?>
 <div class="container">
   <div class="contents">
-    CONTENTS
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : ?>
+            <?php the_post(); ?>
+            <article <?php post_class('article') ?>>
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+            </article>
+        <?php endwhile; ?>
+    <?php endif; ?>
   </div>
   <div class="sub">
     SIDEBAR
