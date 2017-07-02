c66fc2fdc2e74b0b690ce8a1b64586d0a1e71e70
Joji Koike <joji.koike@gmail.com>
2017-07-02 12:44:11 +0900

    URLのカスタム設定対応

--- chef-repo/site-cookbooks/apache/recipes/default.rb
+++ chef-repo/site-cookbooks/apache/recipes/default.rb
@@ -7,7 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 service 'httpd' do
-  action [:enable, :start]
+  action [:enable]
   support_setting = { status: true, restart: true, reload: true}
   supports support_setting
+end
+# For mod rewirte setting
+template '/etc/httpd/conf/httpd.conf' do
+  owner 'root'
+  group 'root'
+  mode 0o644
+  action :create
+  notifies :start, 'service[httpd]', :delayed
 end
