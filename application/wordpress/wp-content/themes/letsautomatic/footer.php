dc8e1e49ee1b98268b03711f750dd7cf56db2dcc
Joji Koike <joji.koike@gmail.com>
2017-07-03 19:45:08 +0900

    ヘッダーバー、フッターバー構成、ボックス間隔調整

--- application/wordpress/wp-content/themes/letsautomatic/footer.php
+++ application/wordpress/wp-content/themes/letsautomatic/footer.php
@@ -1,12 +1,12 @@
 <footer>
-	<div class="copyright">
-		<p>
-			<?php bloginfo('description'); ?>
-		</p>
-		<p>
-			Copyright &copy; <?php bloginfo('name'); ?>
-		</p>
-	</div>
+  <div class="copyright">
+    <p>
+      <?php bloginfo('description'); ?>
+    </p>
+    <p>
+      Copyright &copy; <?php bloginfo('name'); ?>
+    </p>
+  </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
