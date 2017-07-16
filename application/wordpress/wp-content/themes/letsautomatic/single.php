d1aa24e55f6ca774083001532a9eb643fcf38175
Joji Koike <joji.koike@gmail.com>
2017-07-17 07:57:58 +0900

    投稿日、更新日表示実装

--- application/wordpress/wp-content/themes/letsautomatic/single.php
+++ application/wordpress/wp-content/themes/letsautomatic/single.php
@@ -6,6 +6,19 @@
             <?php the_post(); ?>
             <article <?php post_class('article') ?>>
                 <h1><?php the_title(); ?></h1>
+                
+                <div class="article-date">
+                    <time datetime="<?php echo get_the_date('Y-m-d'); ?>">
+                        <i class="fa fa-pencil"></i>
+                        投稿:<?php echo get_the_date('Y年m月d日'); ?>
+                    </time>
+                    <?php if(get_the_modified_date('Ymd') > get_the_date('Ymd')): ?>
+                    |
+                    <time datetime="<?php echo get_the_modified_date('Y-m-d'); ?>">
+                        更新:<?php echo get_the_modified_date('Y年m月d日'); ?>
+                    </time>
+                    <?php endif; ?>
+                </div>
                 <?php if (has_post_thumbnail()): ?>
                 <div class="catch">
                     <?php the_post_thumbnail('large'); ?>
