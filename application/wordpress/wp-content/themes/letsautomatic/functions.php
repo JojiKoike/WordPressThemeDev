3a6096ee385d8e87ebfc231714a4a67bb41e4999
Joji Koike <joji.koike@gmail.com>
2017-07-17 06:15:51 +0900

    アイキャッチ画像表示調整、youtube表示調整、サイト名表示用WebFont設定実装

--- application/wordpress/wp-content/themes/letsautomatic/functions.php
+++ application/wordpress/wp-content/themes/letsautomatic/functions.php
@@ -18,3 +18,23 @@ add_filter('excerpt_more', 'custom_excerpt_more');
 if (!isset($content_width)) {
     $content_width = 747;
 }
+
+// Youtubeのビデオ：<div>でマークアップ
+function ytwrapper($return, $data, $url) {
+    if ($data->provider_name == 'YouTube') {
+        return '<div class="ytvideo">'.$return.'</div>';
+    } else {
+        return $return;
+    }
+}
+add_filter('oembed_dataparse','ytwrapper',10,3);
+
+//// Youtubeのビデオ：キャッシュのクリア
+//function clear_ytwrapper($post_id) {
+//    global $wp_embed;
+//    $wp_embed->delete_oembed_caches($post_id);
+//}
+//add_action('pre_post_update','clear_ytwrapper');
+
+// アイキャッチ画像
+add_theme_support('post-thumbnails');
