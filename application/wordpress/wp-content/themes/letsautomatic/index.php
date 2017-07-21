dfe1c8c09925a56e43e32db318abbb212feaba80
Joji Koike <joji.koike@gmail.com>
2017-07-22 07:38:54 +0900

    一覧に投稿日を表示

--- application/wordpress/wp-content/themes/letsautomatic/index.php
+++ application/wordpress/wp-content/themes/letsautomatic/index.php
@@ -9,6 +9,12 @@
               <a href="<?php the_permalink(); ?>">
                   <img src="<?php echo mythumb('medium'); ?>" alt="">
                 <h1><?php the_title(); ?></h1>
+                <div class="article-date">
+                    <i class="fa fa-pencil"></i>
+                    <time datetime="<?php echo get_the_date('Y-m-d'); ?>">
+                        投稿:<?php echo get_the_date('Y年m月d日'); ?>
+                    </time>
+                </div>
                 <?php the_excerpt(); ?>
               </a>
             </article>
